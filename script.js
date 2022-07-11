9964d634f592679d8c53b8ad503d537a0d1f2fea
sinsect <sin@odin.proj>
2022-07-11 16:02:57 -0400

    add basic add sub mult div functions

---
+++ script.js
@@ -0,0 +1,12 @@
+const add = new function(num1, num2) {
+    return num1 + num2;
+}
+const subtract = new function(num1, num2) {
+    return num1 - num2;
+}
+const multiply = new function(num1, num2) {
+    return num1 * num2;
+}
+const divide = new function(num1, num2) {
+    return num1 / num2;
+}
