ae97cfbbf663a0477521192c6322df513f5ba4c5
sinsect <sin@odin.proj>
2022-07-17 08:11:46 -0400

    implement operate function, fix declaration

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const add = new function(num1, num2) {
+function add(num1, num2) {
     return num1 + num2;
 }
-const subtract = new function(num1, num2) {
+function subtract(num1, num2) {
     return num1 - num2;
 }
-const multiply = new function(num1, num2) {
+function multiply(num1, num2) {
     return num1 * num2;
 }
-const divide = new function(num1, num2) {
+function divide(num1, num2) {
     return num1 / num2;
+}
+function operate(operator, num1, num2){
+    if(operator == '+') {
+        return add(num1, num2);
+    }
+    else if(operator == '-') {
+        return subtract(num1, num2);
+    }
+    else if(operator == '*') {
+        return multiply(num1, num2);
+    }
+    else if(operator == '/') {
+        return divide(num1, num2);
+    }
 }
